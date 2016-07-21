65ec7013beaca478176a36ba51dee5d4cc75ae7e
Art <a.molcanovas@gmail.com>
2016-07-21 19:22:43 +0100

    Rounding to 2 decimal spaces now

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,79 +1,85 @@
-// GLOBAL
 (function () {
-    $("[data-tooltip]").tooltip({
-        container: 'body',
-        html: true
-    });
-
-    $(".nav-tabs [data-toggle=tab]").click(function () {
-        location.hash = $(this).attr("href");
-    });
-
-    if (location.hash) {
-        $("a[href='" + location.hash + "']").click();
+    function roundToTwo(num) {
+        return +(Math.round(num + "e+2") + "e-2");
     }
 
-    if (typeof localStorage !== "undefined") {
-        $("[data-value-setting]").on("change keyup", function () {
-            localStorage.setItem($(this).data("value-setting"), $(this).val());
-        }).each(function () {
-            var val = localStorage.getItem($(this).data("value-setting"));
-            if (val !== null) {
-                $(this).val(val);
-            }
+    // GLOBAL
+    (function () {
+        $("[data-tooltip]").tooltip({
+            container: 'body',
+            html: true
         });
 
-        $("[data-radio-setting]").change(function () {
-            localStorage.setItem($(this).data("radio-setting"), $(this).val());
-        }).each(function () {
-            var selected = localStorage.getItem($(this).data("radio-setting"));
-
-            if (selected !== null && $(this).attr("value") === selected) {
-                $(this).prop("checked", true);
-            }
+        $(".nav-tabs [data-toggle=tab]").click(function () {
+            location.hash = $(this).attr("href");
         });
 
-        $("[data-checkbox-setting]").change(function () {
-            localStorage.setItem($(this).data("checkbox-setting"), $(this).is(":checked") ? 1 : 0);
-        }).each(function () {
-            var checked = localStorage.getItem($(this).data("checkbox-setting")) == 1;
+        if (location.hash) {
+            $("a[href='" + location.hash + "']").click();
+        }
 
-            if (checked != null) {
-                $(this).prop("checked", !!checked);
-            }
-        });
-    }
-})();
+        if (typeof localStorage !== "undefined") {
+            $("[data-value-setting]").on("change keyup", function () {
+                localStorage.setItem($(this).data("value-setting"), $(this).val());
+            }).each(function () {
+                var val = localStorage.getItem($(this).data("value-setting"));
+                if (val !== null) {
+                    $(this).val(val);
+                }
+            });
+
+            $("[data-radio-setting]").change(function () {
+                localStorage.setItem($(this).data("radio-setting"), $(this).val());
+            }).each(function () {
+                var selected = localStorage.getItem($(this).data("radio-setting"));
+
+                if (selected !== null && $(this).attr("value") === selected) {
+                    $(this).prop("checked", true);
+                }
+            });
+
+            $("[data-checkbox-setting]").change(function () {
+                localStorage.setItem($(this).data("checkbox-setting"), $(this).is(":checked") ? 1 : 0);
+            }).each(function () {
+                var checked = localStorage.getItem($(this).data("checkbox-setting")) == 1;
+
+                if (checked != null) {
+                    $(this).prop("checked", !!checked);
+                }
+            });
+        }
+    })();
 
 //Felspire healing PCT
-(function () {
-    var $container = $("#felspire-healing-pct"),
-        $maxHP = $("#_hpt-hpmax"),
-        $hpPlus = $("#_hpt-hpplus"),
-        $hpPack = $container.find("[name=pack]"),
-        $noWaste = $container.find("[name=no-waste]"),
-        $autodrinkL = $("#_res-autodrink-L"),
-        $autodrinkM = $("#_res-autodrink-M"),
-        $autodrinkS = $("#_res-autodrink-S"),
-        recalculate = function () {
-            var maxHP = parseFloat($maxHP.val()),
-                hpPlus = parseFloat($hpPlus.val()),
-                healS = 800 / 100 * (100 + hpPlus),
-                healM = 2000 / 100 * (100 + hpPlus),
-                healL = 8000 / 100 * (100 + hpPlus);
+    (function () {
+        var $container = $("#felspire-healing-pct"),
+            $maxHP = $("#_hpt-hpmax"),
+            $hpPlus = $("#_hpt-hpplus"),
+            $hpPack = $container.find("[name=pack]"),
+            $noWaste = $container.find("[name=no-waste]"),
+            $autodrinkL = $("#_res-autodrink-L"),
+            $autodrinkM = $("#_res-autodrink-M"),
+            $autodrinkS = $("#_res-autodrink-S"),
+            recalculate = function () {
+                var maxHP = parseFloat($maxHP.val()),
+                    hpPlus = parseFloat($hpPlus.val()),
+                    healS = 800 / 100 * (100 + hpPlus),
+                    healM = 2000 / 100 * (100 + hpPlus),
+                    healL = 8000 / 100 * (100 + hpPlus);
 
-            if ($noWaste.is(":checked")) {
-                var hpPack = parseFloat($hpPack.filter(":checked").val());
-                healS += hpPack;
-                healM += hpPack;
-                healL += hpPack;
-            }
+                if ($noWaste.is(":checked")) {
+                    var hpPack = parseFloat($hpPack.filter(":checked").val());
+                    healS += hpPack;
+                    healM += hpPack;
+                    healL += hpPack;
+                }
 
-            $autodrinkL.text(Math.round((maxHP - healL) / maxHP * 100) + "%").attr("data-original-title", (maxHP - healL) + " HP");
-            $autodrinkM.text(Math.round((maxHP - healM) / maxHP * 100) + "%").attr("data-original-title", (maxHP - healM) + " HP");
-            $autodrinkS.text(Math.round((maxHP - healS) / maxHP * 100) + "%").attr("data-original-title", (maxHP - healS) + " HP");
-        };
+                $autodrinkL.text(roundToTwo((maxHP - healL) / maxHP * 100) + "%").attr("data-original-title", (maxHP - healL) + " HP");
+                $autodrinkM.text(roundToTwo((maxHP - healM) / maxHP * 100) + "%").attr("data-original-title", (maxHP - healM) + " HP");
+                $autodrinkS.text(roundToTwo((maxHP - healS) / maxHP * 100) + "%").attr("data-original-title", (maxHP - healS) + " HP");
+            };
 
-    $container.find("input").on("change keyup", recalculate);
-    recalculate();
+        $container.find("input").on("change keyup", recalculate);
+        recalculate();
+    })();
 })();
