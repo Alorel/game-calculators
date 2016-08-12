38e6211811bd10d76dc5a26128f97e9c101c1e8e
Art <a.molcanovas@gmail.com>
2016-08-12 13:18:34 +0100

    re-added some of the setting stuff I accidentaly removed - woops!

--- assets/js/global.js
+++ assets/js/global.js
@@ -33,4 +33,47 @@
 
         return ret;
     };
+
+    $(document).ready(function () {
+        $("[data-tooltip]").tooltip({
+            container: 'body',
+            html: true
+        });
+
+        $(".nav-tabs [data-toggle=tab]").click(function () {
+            location.hash = $(this).attr("href");
+        });
+
+        if (location.hash) {
+            $("a[href='" + location.hash + "']").click();
+        }
+        $("[data-value-setting]").on("change keyup", function () {
+            localStorage.setItem($(this).data("value-setting"), $(this).val());
+        }).each(function () {
+            var val = localStorage.getItem($(this).data("value-setting"));
+            if (val !== null) {
+                $(this).val(val);
+            }
+        });
+
+        $("[data-radio-setting]").change(function () {
+            localStorage.setItem($(this).data("radio-setting"), $(this).val());
+        }).each(function () {
+            var selected = localStorage.getItem($(this).data("radio-setting"));
+
+            if (selected !== null && $(this).attr("value") === selected) {
+                $(this).prop("checked", true);
+            }
+        });
+
+        $("[data-checkbox-setting]").change(function () {
+            localStorage.setItem($(this).data("checkbox-setting"), $(this).is(":checked") ? 1 : 0);
+        }).each(function () {
+            var checked = localStorage.getItem($(this).data("checkbox-setting")) == 1;
+
+            if (checked != null) {
+                $(this).prop("checked", !!checked);
+            }
+        });
+    });
 })(Math, typeof localStorage !== "undefined" ? localStorage : null, location, window, decodeURIComponent);
